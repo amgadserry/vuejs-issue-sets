e4ef11bbc7876f247bd1ece9aebf31ff0a4084f7
amgadserry <amgad.serry@hotmail.com>
2017-03-21 12:27:12 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   div
-    div.test-block(@click="toggleSelected") {{isSelected}}
+    div.test-block(@click="toggleSelected") {{user.isSelected}}
 </template>
 
 <script>
@@ -16,11 +16,6 @@
     methods: {
       toggleSelected () {
         this.$store.commit('toggleSelected', this.user)
-      }
-    },
-    computed: {
-      isSelected () {
-        return this.$store.state.User.selectedUsers.has(this.user)
       }
     }
   }
